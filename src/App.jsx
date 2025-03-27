92b48f559471cde7368600c86a6a831f904a51b4
Ahmad <ahmad@Ahmads-MacBook-Pro.local>
2025-03-27 13:19:20 +0500

    Initial commit with project files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./components/GlobalCSS.css";
+import "./App.css";
+import EmailForm from "./pages/createAccount/components/EmailForm";
+
+function App() {
+  return (
+    <>
+      <EmailForm></EmailForm>
+    </>
+  );
+}
+
+export default App;
